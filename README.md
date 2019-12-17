4a79a2274d011b5fb3a878ddbabc49f06b43c7f8
Sean Bermejo <sean.bermejo@gmail.com>
2019-12-18 00:11:07 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+* Django-Environ template
+
+This template creates a simple template structure with django-environ. 
+
+
+** Structure **
+```
+- config/
+ - settings/
+   - __init__.py
+   - base.py
+   - platforms/
+     - local.py
+ - urls.py
+ - wsgi.py
+- manage.py
+``**
+
+** Installation **
+
+```
+$ django-admin startproject project --template django-environ-structure
+```
---
+++ config/settings/__init__.py
@@ -0,0 +1,42 @@
+import os
+import sys
+
+import environ
+
+from split_settings.tools import optional
+from split_settings.tools import include
+
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+base = environ.Path(__file__) - 3
+BASE_DIR = base()
+
+env = environ.Env()
+if os.path.exists(base('.env')):
+    print(f'Loading env file: f{base(".env")}')
+
+if os.getenv('DJANGO_SETTINGS_MODULE') == 'config.settings':
+    platform = env.str('PLATFORM', default='local')
+
+    include(
+        'base.py',
+        optional(os.path.join('platform', f'{platform}.py')),
+        scope=globals(),
+    )
+
+# Covers regular testing and django-coverage
+tests = [
+    'test',
+    'test_coverage',
+    'pytest',
+]
+TESTING = any(t in ''.join(sys.argv) for t in tests) or env.bool('TESTING', default=False)
+# Use this to set models to managed=True by settings
+# MANAGED=true
+# to .env file
+# If running tests this defaults to True
+MANAGED = TESTING or env.bool('MANAGED', default=False)
+if TESTING:
+    include(
+        optional('tests.py'),
+    )
